91199ca5ff630159c7488d3ce5fc1775c98429cf
KZyred <KZyred@DESKTOP-JUQH88D>
2023-09-29 09:56:02 +0700

    first commit

---
+++ db/db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS empresadb;
+
+USE empresadb;
+
+CREATE TABLE empleado (
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) DEFAULT NULL,
+    salario DECIMAL(8,2) DEFAULT NULL,
+    PRIMARY KEY(id)
+);
+
+SHOW TABLES;
+
+DESCRIBE empleado;
+
+INSERT INTO empleado VALUES ("Yostin", 77000.99);
+
+SELECT * FROM empleado;
+
